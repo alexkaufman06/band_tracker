db9e6d49942bf96d9c2574e7fe7fa22ba1a8c771
Alex Kaufman <alexkaufman06@gmail.com>
2015-01-30 10:28:14 -0800

    Add sinatra usability and a new bands.erb page

--- app.rb
+++ app.rb
@@ -3,5 +3,20 @@ Bundler.require(:default)
 Dir[File.dirname(__FILE__) + "/lib/*.rb"].each { |file| require file }
 
 get('/') do
+  @bands = Band.all()
+  @venues = Venue.all()
   erb(:index)
+end
+
+post('/') do
+  band_name = params.fetch("band_name")
+  Band.create({ :name => band_name })
+  venue_name = params.fetch("venue_name")
+  Venue.create({ :name => venue_name })
+  redirect('/')
+end
+
+get('/band/:id') do
+  @band = Band.find(params.fetch("id").to_i())
+  erb(:bands)
 end
