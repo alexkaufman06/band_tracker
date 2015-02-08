365d39891228e15297109e5a43d233f768685699
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-08 14:16:45 -0800

    Add syntactic sugar to bands.rb

--- lib/bands.rb
+++ lib/bands.rb
@@ -1,11 +1,11 @@
 class Band < ActiveRecord::Base
   has_and_belongs_to_many :venues
-  validates(:name, :presence => true)
+  validates :name, :presence => true 
   before_save :capitalize_name
 
 private
 
   def capitalize_name
-    self.name=(name.capitalize)
+    self.name = name.capitalize
   end
 end
